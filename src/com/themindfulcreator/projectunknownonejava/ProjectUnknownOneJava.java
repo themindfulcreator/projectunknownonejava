ebe7284d75ff2a41d30d391e56be1b4ad33becad
themindfulcreator <themindfulcreator@gmail.com>
2021-05-22 23:22:30 +0800

    Initial commit for projectunknownonejava project.

---
+++ src/com/themindfulcreator/projectunknownonejava/ProjectUnknownOneJava.java
@@ -0,0 +1,7 @@
+package com.themindfulcreator.projectunknownonejava;
+
+public class ProjectUnknownOneJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
