f4a381fb3ea414bf744b1b4ba895cadfb0996dcc
themindfulcreator <themindfulcreator@gmail.com>
2021-05-22 23:24:20 +0800

    added (new)

--- src/com/themindfulcreator/projectunknownonejava/ProjectUnknownOneJava.java
+++ src/com/themindfulcreator/projectunknownonejava/ProjectUnknownOneJava.java
@@ -2,6 +2,6 @@ package com.themindfulcreator.projectunknownonejava;
 
 public class ProjectUnknownOneJava {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World(new)!");
     }
 }
